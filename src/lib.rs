907b096855bb632197baedb6166a82d4459d9ea6
Christian Thiel <christian@hansetag.com>
2024-09-26 12:28:26 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-#![warn(
-    missing_debug_implementations,
-    rust_2018_idioms,
-    unreachable_pub,
-)]
+#![warn(missing_debug_implementations, rust_2018_idioms, unreachable_pub)]
 #![forbid(unsafe_code)]
 
 pub use prost_types;
